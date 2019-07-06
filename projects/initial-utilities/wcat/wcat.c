846c5f9420bbf5c7de24819ea6c4d0433c3b364c
xxyzz <elephantpolar@gmail.com>
2019-07-06 23:00:04 +0800

    wcat

---
+++ projects/initial-utilities/wcat/wcat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>    // exit
+#include <string.h>    // strerror
+
+#define BUFFER_SIZE 1024
+
+int 
+main(int argc, char *argv[])
+{
+    FILE *fp;
+    char buff[BUFFER_SIZE];
+
+    for (size_t i = 1; i < argc; i++)
+    {
+        if ((fp = fopen(argv[i], "r")) == NULL)
+        {
+            printf("wcat: cannot open file\n");
+            exit(EXIT_FAILURE);
+        }
+
+        while (fgets(buff, BUFFER_SIZE, fp))
+            printf("%s", buff);
+
+        errno = 0;
+        if (fclose(fp) != 0) {
+            strerror(errno);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
