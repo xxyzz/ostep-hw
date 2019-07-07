55ff25fb790f60cd77bbb1f367d7205c3bf87433
xxyzz <elephantpolar@gmail.com>
2019-07-07 09:48:44 +0800

    wgrep

---
+++ projects/initial-utilities/wgrep/wgrep.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>    // exit
+#include <string.h>    // strstr
+
+int
+main(int argc, char *argv[])
+{
+    FILE *fp = NULL;
+    char *line = NULL;
+    size_t linecap = 0;
+
+    if (argc <= 1) {
+        printf("wgrep: searchterm [file ...]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (argc >= 3 && (fp = fopen(argv[2], "r")) == NULL) {
+        printf("wgrep: cannot open file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (argc == 2)
+        fp = stdin;
+
+    while (getline(&line, &linecap, fp) > 0)
+        if (strstr(line, argv[1]))
+            printf("%s", line);
+
+    return 0;
+}
