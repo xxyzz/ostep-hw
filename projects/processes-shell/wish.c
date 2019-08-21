c7cb64ef7e08ea8db4280e35134d4c4a497da7f5
xxyzz <elephantpolar@gmail.com>
2019-08-21 22:00:58 +0800

    impliment cd & move code to functions

--- projects/processes-shell/wish.c
+++ projects/processes-shell/wish.c
@@ -1,9 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>      // exit
 #include <sys/types.h>
-#include <unistd.h>      // fork, exec, access, _exit
+#include <unistd.h>      // fork, exec, access, exit, chdir
 #include <sys/wait.h>    // wait
 #include "wish.h"
+
+void
+parseInput(char *line, ssize_t nread, char *args[], int *args_num)
+{
+    // remove newline
+    if (line[nread - 1] == '\n')
+        line[nread - 1] = '\0';
+
+    while ((args[*args_num] = strsep(&line, " ")) != NULL)
+        (*args_num)++;
+}
+
+int
+searchPath(char *paths[], char path[], char *firstArg)
+{
+    // search executable file in path
+    int i = 0;
+    while (paths[i] != NULL)
+    {
+        strcpy(path, paths[i]);
+        strcat(path, "/");
+        if (access(strcat(path, firstArg), X_OK) == 0)
+        {
+            firstArg = path;
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+void
+executeCommands(char *args[], int args_num, char *paths[], char *line, FILE *in)
+{
+    // check built-in commands first
+    if (strcmp(args[0], "exit") == 0)
+    {
+        if (args_num > 1)
+            printError();
+        else
+        {
+            free(line);
+            fclose(in);
+            exit(EXIT_SUCCESS);
+        }
+    }
+    else if (strcmp(args[0], "cd") == 0)
+    {
+        if (args_num == 1 || args_num > 2)
+            printError();
+        else if (chdir(args[1]) == -1)
+            printError();  
+    }
+    else if (strcmp(args[0], "path") == 0)
+    {
+        size_t i = 0;
+        for ( ; i < args_num; i++)
+            paths[i] = args[i+1];
+
+        paths[i+1] = NULL;
+    }
+    else
+    {
+        // not built-in commands
+        char path[BUFF_SIZE] = "";
+        if (searchPath(paths, path, args[0]))
+        {
+            pid_t pid = fork();
+            if (pid == -1)
+                printError();
+            else if (pid == 0)
+            {
+                // child process    
+                if (execv(path, args) == -1)
+                    printError();  
+            }
+            else
+            {
+                // parent process
+                // wait all children
+                while(wait(NULL) != -1)
+                    ;
+            }
+        }
+        else
+            printError();    // not fond in path
+    }
+}
 
 int
 main(int argc, char *argv[])
@@ -14,7 +102,6 @@ main(int argc, char *argv[])
     char *line = NULL;
     size_t linecap = 0;
     ssize_t nread;
-    pid_t pid;
     char *paths[BUFF_SIZE] = {"/bin", NULL};
 
     if (argc > 1)
@@ -33,76 +120,11 @@ main(int argc, char *argv[])
 
         if ((nread = getline(&line, &linecap, in)) > 0)
         {
-            // remove newline
-            if (line[nread - 1] == '\n')
-                line[nread - 1] = '\0';
-
             char *args[BUFF_SIZE];
             int args_num = 0;
-            while ((args[args_num] = strsep(&line, " ")) != NULL)
-                args_num++;
+            parseInput(line, nread, args, &args_num);
 
-            // check built-in commands first
-            if (strcmp(args[0], "exit") == 0)
-            {
-                if (args_num > 1)
-                    printError();
-                else
-                {
-                    free(line);
-                    fclose(in);
-                    exit(EXIT_SUCCESS);
-                }
-            }
-            else if (strcmp(args[0], "path") == 0)
-            {
-                size_t i = 0;
-                for ( ; i < args_num; i++)
-                    paths[i] = args[i+1];
-
-                paths[i+1] = NULL;
-            }
-            else
-            {
-                // find in path
-                int i = 0, found = 0;
-                char path[BUFF_SIZE] = "";
-                while (paths[i] != NULL)
-                {
-                    strcpy(path, paths[i]);
-                    strcat(path, "/");
-                    if (access(strcat(path, args[0]), X_OK) == 0)
-                    {
-                        args[0] = path;
-                        found = 1;
-                        break;
-                    }
-                    i++;
-                }
-
-                if (!found)
-                {
-                    printError();
-                    continue;
-                }
-
-                pid = fork();
-                if (pid == -1)
-                    printError();
-                else if (pid == 0)
-                {
-                    // child process    
-                    if (execv(path, args) == -1)
-                        printError();  
-                }
-                else
-                {
-                    // parent process
-                    // wait all children
-                    while(wait(NULL) != -1)
-                        ;
-                }
-            }
+            executeCommands(args, args_num, paths, line, in);
         }
         else if (feof(in) != 0)
         {
