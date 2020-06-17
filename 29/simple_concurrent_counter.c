2f90ecf02f314f57970d21405f89983638d086fc
xxyzz <gitpull@protonmail.com>
2020-06-17 21:13:01 +0800

    update simple counter
    still can't get the results(over five seconds) in the book

--- 29/simple_concurrent_counter.c
+++ 29/simple_concurrent_counter.c
@@ -1,90 +1,114 @@
+#ifdef Linux
 #define _GNU_SOURCE
+#include <sched.h> // CPU_ZERO
+#endif
+
+#ifdef FreeBSD
+#include <malloc_np.h>
+#include <sys/_cpuset.h>
+#include <sys/cpuset.h>
+#ifndef cpu_set_t
+#define cpu_set_t cpuset_t
+#endif
+#endif
+
+#include "thread_helper.h"
 #include <stdio.h>
-#include <pthread.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <unistd.h>
+#include <stdlib.h>   // malloc, free
+#include <sys/time.h> // gettimeofday
+#include <unistd.h>   // sysconf
 #define ONE_MILLION 1000000
 
 typedef struct __counter_t {
-    int             value;
-    pthread_mutex_t lock;
+  pthread_mutex_t lock;
+  int value;
+  char pad[sizeof(pthread_mutex_t) - sizeof(int)];
 } counter_t;
 
 typedef struct __myarg_t {
-    counter_t *counter;
-    cpu_set_t set;
-    int       threads;
+  counter_t *counter;
+  cpu_set_t set;
+  int threads;
+  char pad[sizeof(counter_t *) - sizeof(int)];
 } myarg_t;
 
-void init(counter_t *c) {
-    c->value = 0;
-    pthread_mutex_init(&c->lock, NULL);
+static void init(counter_t *c) {
+  c->value = 0;
+  Pthread_mutex_init(&c->lock, NULL);
 }
 
-void increment(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
-    c->value++;
-    pthread_mutex_unlock(&c->lock);
+static void increment(counter_t *c) {
+  Pthread_mutex_lock(&c->lock);
+  c->value++;
+  Pthread_mutex_unlock(&c->lock);
 }
 
-void decrement(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
-    c->value--;
-    pthread_mutex_unlock(&c->lock);
+static int get(counter_t *c) {
+  Pthread_mutex_lock(&c->lock);
+  int rc = c->value;
+  Pthread_mutex_unlock(&c->lock);
+  return rc;
 }
 
-int get(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
-    int rc = c->value;
-    pthread_mutex_unlock(&c->lock);
-    return rc;
-}
+static void *thread_function(void *arg) {
+  myarg_t *m = (myarg_t *)arg;
+  int s;
+  s = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &m->set);
+  if (s != 0)
+    handle_error_en(s, "pthread_setaffinity_np");
 
-void *thread_function(void *arg) {
-    myarg_t *m = (myarg_t *) arg;
-    if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &m->set) == -1) {
-        printf("Set CPU affinity error\n");
-        pthread_exit(0);
-    }
-    for (int i = 0; i < ONE_MILLION / m->threads; i++) {
-        increment(m->counter);
-    }
-    pthread_exit(0);
+  for (int i = 0; i < ONE_MILLION / m->threads; i++)
+    increment(m->counter);
+
+  pthread_exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[]) {
-    cpu_set_t set;
+  cpu_set_t set;
+  int cpus = (int)sysconf(_SC_NPROCESSORS_ONLN), s;
+  if (cpus == -1)
+    handle_error_en(cpus, "sysconf");
+
+  for (int i = 1; i <= cpus; i++) {
     CPU_ZERO(&set);
-    for (int i = 1; i < 5; i++) {
-        for(int m = 0; m < i; m++) {
-            CPU_SET(m, &set);
-        }   
-        printf("%d CPUs\n", i);
-        for (int l = 1; l < 5; l++) {
-            pthread_t threads[l - 1];
-            struct timeval start, end;
-            counter_t *counter = malloc(sizeof(counter_t));
-            if (counter == NULL) {
-                return -1;
-            }
-            init(counter);
-            myarg_t args;
-            args.counter = counter;
-            args.set = set;
-            args.threads = l;
-            gettimeofday(&start, NULL);
-            for(int j = 0; j < l; j++) {
-                pthread_create(&threads[j], NULL, &thread_function, &args);
-            }   
-            for(int k = 0; k < l; k++) {
-                pthread_join(threads[k], NULL);
-            }
-            gettimeofday(&end, NULL);
-            printf("%d threads\n", l);
-            printf("Time (seconds): %f\n\n", (float) (end.tv_usec - start.tv_usec + (end.tv_sec - start.tv_sec) * ONE_MILLION) / ONE_MILLION);
-            free(counter);
-        }
+    for (int m = 0; m < i; m++)
+      CPU_SET(m, &set);
+
+    printf("%d CPUs\n", i);
+    for (int l = 1; l <= cpus; l++) {
+      pthread_t *threads = malloc((size_t)l * sizeof(pthread_t));
+      if (threads == NULL)
+        handle_error_en(errno, "malloc");
+      struct timeval start, end;
+      counter_t *counter = malloc(sizeof(counter_t));
+      if (counter == NULL)
+        handle_error_en(errno, "malloc");
+      init(counter);
+      myarg_t args;
+      args.counter = counter;
+      args.set = set;
+      args.threads = l;
+
+      s = gettimeofday(&start, NULL);
+      if (s != 0)
+        handle_error_en(s, "gettimeofday");
+      for (int j = 0; j < l; j++)
+        pthread_create(&threads[j], NULL, &thread_function, &args);
+      for (int k = 0; k < l; k++)
+        pthread_join(threads[k], NULL);
+      s = gettimeofday(&end, NULL);
+      if (s != 0)
+        handle_error_en(s, "gettimeofday");
+
+      printf("%d threads\n", l);
+      printf("global count: %d\n", get(counter));
+      printf("Time (seconds): %f\n\n",
+             ((double)(end.tv_usec - start.tv_usec) / ONE_MILLION +
+              (double)(end.tv_sec - start.tv_sec)));
+      Pthread_mutex_destroy(&counter->lock);
+      free(counter);
+      free(threads);
     }
-    return 0;
+  }
+  return 0;
 }
---
+++ 29/thread_helper.h
@@ -0,0 +1,28 @@
+#ifndef _thread_helper_h
+#define _thread_helper_h
+
+#ifdef FreeBSD
+#include <pthread_np.h>
+#endif
+#include <assert.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define handle_error_en(en, msg)                                               \
+  do {                                                                         \
+    errno = en;                                                                \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+#define Pthread_create(thread, attr, start_routine, arg)                       \
+  assert(pthread_create(thread, attr, start_routine, arg) == 0)
+#define Pthread_join(thread, value_ptr)                                        \
+  assert(pthread_join(thread, value_ptr) == 0)
+#define Pthread_cancel(thread) assert(pthread_cancel(thread) == 0)
+#define Pthread_mutex_init(m, attr) assert(pthread_mutex_init(m, attr) == 0)
+#define Pthread_mutex_lock(m) assert(pthread_mutex_lock(m) == 0)
+#define Pthread_mutex_unlock(m) assert(pthread_mutex_unlock(m) == 0)
+#define Pthread_mutex_destroy(m) assert(pthread_mutex_destroy(m) == 0)
+
+#endif // _thread_helper_h
