e1a0f1ddf7244b48c169533831675de8f59b4c1c
xxyzz <gitpull@protonmail.com>
2021-04-09 09:23:12 +0800

    rewrite reverse.c, close #9

--- projects/initial-reverse/reverse.c
+++ projects/initial-reverse/reverse.c
@@ -1,132 +1,77 @@
-#include <stdio.h>
-#include <stdlib.h>      // exit, malloc
-#include <sys/stat.h>    // fstat
-#include <string.h>      // strcpy, strcmp
+#include <stdio.h>    // getline, fileno, fopen, fclose, fprintf
+#include <stdlib.h>   // exit, malloc
+#include <string.h>   // strdup
+#include <sys/stat.h> // fstat
+#include <sys/types.h>
 
-typedef struct linkedList
-{
-    char *line;
-    struct linkedList *next;
+#define handle_error(msg)                                                      \
+  do {                                                                         \
+    fprintf(stderr, msg);                                                      \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+typedef struct linkedList {
+  char *line;
+  struct linkedList *next;
 } LinkedList;
 
-int
-main(int argc, char *argv[])
-{
-    FILE *in = NULL, *out = NULL;
-    in  = stdin;
-    out = stdout;
+int main(int argc, char *argv[]) {
+  FILE *in = NULL, *out = NULL;
+  in = stdin;
+  out = stdout;
 
-    if (argc == 2) {
-        if ((in = fopen(argv[1], "r")) == NULL) {
-            fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
-            exit(EXIT_FAILURE);
-        }
+  if (argc == 2 && (in = fopen(argv[1], "r")) == NULL) {
+    fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
+
+  if (argc == 3) {
+    if ((in = fopen(argv[1], "r")) == NULL) {
+      fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
+      exit(EXIT_FAILURE);
     }
-    
-    if (argc == 3) {
-        if ((in = fopen(argv[1], "r")) == NULL) {
-            fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
-            exit(EXIT_FAILURE);
-        }
-        if ((out = fopen(argv[2], "w")) == NULL) {
-            fprintf(stderr, "reverse: cannot open file '%s'\n", argv[2]);
-            exit(EXIT_FAILURE);
-        }
-
-        struct stat *s1 = malloc(sizeof(struct stat)), *s2 = malloc(sizeof(struct stat));
-        if (s1 == NULL || s2 == NULL) {
-            fprintf(stderr, "malloc failed\n");
-            exit(EXIT_FAILURE);
-        }
-
-        if (fstat(fileno(in), s1) != 0) {
-            free(s1);
-            free(s2);
-            fprintf(stderr, "reverse: fstat error\n");
-            exit(EXIT_FAILURE);
-        }
-        if (fstat(fileno(out), s2) != 0) {
-            free(s1);
-            free(s2);
-            fprintf(stderr, "reverse: fstat error\n");
-            exit(EXIT_FAILURE);
-        }
-
-        if (s1->st_ino == s2->st_ino) {
-            free(s1);
-            free(s2);
-            fprintf(stderr, "reverse: input and output file must differ\n");
-            exit(EXIT_FAILURE);
-        }
-
-        free(s1);
-        free(s2);
+    if ((out = fopen(argv[2], "w")) == NULL) {
+      fprintf(stderr, "reverse: cannot open file '%s'\n", argv[2]);
+      exit(EXIT_FAILURE);
     }
 
-    if (argc > 3) {
-        fprintf(stderr, "usage: reverse <input> <output>\n");
-        exit(EXIT_FAILURE);
+    struct stat sb1, sb2;
+    if (fstat(fileno(in), &sb1) == -1 || fstat(fileno(out), &sb2) == -1)
+      handle_error("reverse: fstat error\n");
+    if (sb1.st_ino == sb2.st_ino)
+      handle_error("reverse: input and output file must differ\n");
+  } else if (argc > 3)
+    handle_error("usage: reverse <input> <output>\n");
+
+  LinkedList *head = NULL;
+  char *line = NULL;
+  size_t len = 0;
+  // store lines to list in reverse order
+  while (getline(&line, &len, in) != -1) {
+    LinkedList *node = malloc(sizeof(LinkedList));
+    if (node == NULL) {
+      free(line);
+      handle_error("reverse: malloc failed\n");
     }
-
-    LinkedList *head = malloc(sizeof(LinkedList)), *curr = NULL, *prev = NULL;
-
-    if (head == NULL) {
-        fprintf(stderr, "reverse: malloc failed\n");
-        exit(EXIT_FAILURE);
+    if ((node->line = strdup(line)) == NULL) {
+      free(line);
+      handle_error("reverse: strdup failed\n");
     }
-    head->line = NULL;
-    head->next = NULL;
-    curr = head;
-    char *line = NULL;
-    size_t linecap = 0;
+    node->next = head;
+    head = node;
+  }
 
-    // store lines to list
-    while (getline(&line, &linecap, in) > 0) {
-        if (head->line == NULL) {
-            if ((head->line = malloc(linecap)) == NULL) {
-                fprintf(stderr, "reverse: malloc failed\n");
-                exit(EXIT_FAILURE);
-            }
-            strcpy(head->line, line);
-        } else {
-            LinkedList *temp = malloc(sizeof(LinkedList));
-            if (temp == NULL) {
-                fprintf(stderr, "reverse: malloc failed\n");
-                exit(EXIT_FAILURE);
-            }
-            if ((temp->line = malloc(linecap)) == NULL) {
-                fprintf(stderr, "reverse: malloc failed\n");
-                exit(EXIT_FAILURE);
-            }
-            strcpy(temp->line, line);
+  // print reversed lines
+  while (head != NULL) {
+    LinkedList *temp = head;
+    fprintf(out, "%s", head->line);
+    head = head->next;
+    free(temp->line);
+    free(temp);
+  }
 
-            curr->next = temp;
-            curr = temp;
-        }
-    }
-
-    // reverse list
-    curr = head;
-    while (curr != NULL) {
-        LinkedList *temp = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = temp;
-    }
-    head = prev;
-
-    // print reversed lines
-    while (head != NULL) {
-        LinkedList *temp = head;
-        fprintf(out, "%s", head->line);
-        head = head->next;
-        free(temp->line);
-        free(temp);
-    }
-
-    free(line);
-    fclose(in);
-    fclose(out);
-
-    return 0;
+  free(line);
+  fclose(in);
+  fclose(out);
+  return 0;
 }
