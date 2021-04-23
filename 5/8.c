0472a6b004c21c860c92e2d4f1a73372ee828901
xxyzz <gitpull@protonmail.com>
2021-04-23 12:25:16 +0800

    connect the first child's stdout to the second one's stdin with pipe

--- 5/8.c
+++ 5/8.c
@@ -1,48 +1,55 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>   // exit
 #include <string.h>   // strlen
 #include <sys/wait.h> // waitpid
-#include <unistd.h>   // fork, pipe, close, write
+#include <unistd.h>   // fork, pipe, close, write, dup2
+
+#define errExit(msg)                                                           \
+  do {                                                                         \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+#define BUFFSIZE 4096
 
 int main() {
   int pipefd[2];
-  char buf;
-
-  if (pipe(pipefd) == -1) {
-    perror("pipe");
-    exit(EXIT_FAILURE);
-  }
+  if (pipe(pipefd) == -1)
+    errExit("pipe");
 
   pid_t rc[2];
   rc[0] = fork();
-
-  if (rc[0] < 0) {
-    // fork failed; exit
-    fprintf(stderr, "fork failed\n");
-    exit(EXIT_FAILURE);
-  } else if (rc[0] == 0) {
+  if (rc[0] < 0)
+    errExit("fork");
+  else if (rc[0] == 0) {
     close(pipefd[0]); /* Close unused read end */
-    char str[] = "The first child writes: I'm the first child.";
-    write(pipefd[1], str, strlen(str));
-    close(pipefd[1]);        /* Reader will see EOF */
-    waitpid(rc[1], NULL, 0); /* Wait for the second child */
+    if (pipefd[1] != STDOUT_FILENO) {
+      if (dup2(pipefd[1], STDOUT_FILENO) != STDOUT_FILENO)
+        errExit("dup2");
+      close(pipefd[1]);
+    }
+    printf("First child says hello.");
   } else {
     rc[1] = fork();
-    if (rc[1] < 0) {
-      // fork failed; exit
-      fprintf(stderr, "fork failed\n");
-      exit(EXIT_FAILURE);
-    } else if (rc[1] == 0) {
+    if (rc[1] < 0)
+      errExit("fork");
+    else if (rc[1] == 0) {
       close(pipefd[1]); /* Close unused write end */
-      // reads this string a byte at a time from the pipe and echoes it on
-      // standard output.
-      while (read(pipefd[0], &buf, 1) > 0) {
-        write(STDOUT_FILENO, &buf, 1);
+      if (pipefd[0] != STDIN_FILENO) {
+        if (dup2(pipefd[0], STDIN_FILENO) != STDIN_FILENO)
+          errExit("dup2");
+        close(pipefd[0]);
       }
-      write(STDOUT_FILENO, "\n", 1);
-      close(pipefd[0]);
-    } else
-      waitpid(rc[0], NULL, 0);
+      char buf[BUFFSIZE];
+      read(STDIN_FILENO, buf, BUFFSIZE);
+      printf("Second child print: %s\n", buf);
+    } else {
+      if (waitpid(rc[0], NULL, 0) == -1)
+        errExit("waitpid");
+      if (waitpid(rc[1], NULL, 0) == -1)
+        errExit("waitpid");
+    }
   }
   return 0;
 }
