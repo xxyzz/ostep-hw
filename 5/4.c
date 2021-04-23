d4f4ecc74b6246c21cd3be0fcc74c5845ede28cf
xxyzz <gitpull@protonmail.com>
2021-04-23 08:42:25 +0800

    call exec functions in forked processes sequentially

--- 5/4.c
+++ 5/4.c
@@ -1,26 +1,65 @@
-// #define _GNU_SOURCE    // for execvpe()
+#define _GNU_SOURCE // for execvpe()
+#if __has_include(<features.h>)
+#include <features.h>
+#endif
 #include <stdio.h>
-#include <stdlib.h>  // exit
-#include <unistd.h>  // fork, exec*
+#include <sys/wait.h>
+#include <unistd.h> // fork, exec*
 
 int main() {
+  char *argvs[] = {"ls", "-l", "-a", "-h", NULL};
+  char *envp[] = {NULL};
   int rc = fork();
 
-  if (rc < 0) {
-    // fork failed; exit
-    fprintf(stderr, "fork failed\n");
-    exit(EXIT_FAILURE);
-  } else if (rc == 0) {
-    char *argvs[] = {"ls", "-l", "-a", "-h", NULL};
-    char *envp[] = {NULL};
-
+  if (rc == 0) {
+    printf("execl:\n");
     execl("/bin/ls", "ls", "-l", "-a", "-h", NULL);
-    execlp("ls", "ls", "-l", "-a", "-h", NULL);
-    execle("/bin/ls", "ls", "-l", "-a", "-h", NULL, envp);
-
-    execv("/bin/ls", argvs);
-    execvp("ls", argvs);
-    // execvpe("ls", argv, envp);    // GNU extension
+  } else {
+    wait(NULL);
+    rc = fork();
+    if (rc == 0) {
+      printf("\nexeclp:\n");
+      execlp("ls", "ls", "-l", "-a", "-h", NULL);
+    } else {
+      wait(NULL);
+      rc = fork();
+      if (rc == 0) {
+        printf("\nexecle:\n");
+        execle("/bin/ls", "ls", "-l", "-a", "-h", NULL, envp);
+      } else {
+        wait(NULL);
+        rc = fork();
+        if (rc == 0) {
+          printf("\nexecv:\n");
+          execv("/bin/ls", argvs);
+        } else {
+          wait(NULL);
+          rc = fork();
+          if (rc == 0) {
+            printf("\nexecvp:\n");
+            execvp("ls", argvs);
+          } else {
+            wait(NULL);
+            rc = fork();
+            if (rc == 0) {
+              printf("\nexecve:\n");
+              execve("/bin/ls", argvs, envp);
+            } else {
+              wait(NULL);
+#ifdef __GNU_LIBRARY__
+              rc = fork();
+              if (rc == 0) {
+                printf("\nexecvpe:\n");
+                execvpe("ls", argvs, envp);
+              } else
+                wait(NULL);
+#endif
+            }
+          }
+        }
+      }
+    }
   }
+
   return 0;
 }
