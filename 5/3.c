bcd3b6b80d2784f2fc15822665638421b3b53107
xxyzz <gitpull@protonmail.com>
2021-04-24 10:27:02 +0800

    no need to use sigaction()

--- 5/3.c
+++ 5/3.c
@@ -1,27 +1,19 @@
-#include <signal.h>  // kill
+#include <signal.h> // kill
 #include <stdio.h>
-#include <stdlib.h>  // exit
-#include <unistd.h>  // getpid, fork, pause
-
-static void sigcont_handler() { printf("goodbye\n"); }
+#include <stdlib.h> // exit
+#include <unistd.h> // getpid, fork, pause
 
 int main() {
-  int parent_pid = getpid();
   int rc = fork();
-  struct sigaction act;
-  act.sa_handler = sigcont_handler;
-
   if (rc < 0) {
-    // fork failed; exit
     fprintf(stderr, "fork failed\n");
     exit(EXIT_FAILURE);
   } else if (rc == 0) {
     printf("hello\n");
-    kill(parent_pid, SIGCONT);
+    kill(getpid(), SIGCONT);
   } else {
-    sigaction(SIGCONT, &act, NULL);
     pause();
-    wait(NULL);
+    printf("goodbye\n");
   }
   return 0;
 }
