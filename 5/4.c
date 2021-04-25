c2a4a615b2518910d469c8745d315233d8b36213
xxyzz <gitpull@protonmail.com>
2021-04-25 10:19:58 +0800

    add fexecve

--- 5/4.c
+++ 5/4.c
@@ -2,64 +2,105 @@
 #if __has_include(<features.h>)
 #include <features.h>
 #endif
-#include <stdio.h>
+#include <fcntl.h>  // open
+#include <stdio.h>  // perror
+#include <stdlib.h> // exit
 #include <sys/wait.h>
-#include <unistd.h> // fork, exec*
+#include <unistd.h> // fork, exec*, fexecve
 
-int main() {
-  char *argvs[] = {"ls", "-l", "-a", "-h", NULL};
-  char *envp[] = {NULL};
-  int rc = fork();
+#define errExit(msg)                                                           \
+  do {                                                                         \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
 
-  if (rc == 0) {
+int main() { // APUE 8.10
+  char *argv[] = {"ls", "-l", "-a", "-h", (char *)NULL};
+  char *envp[] = {"PATH=/bin:/usr/bin", NULL};
+
+  pid_t cpid = fork();
+  if (cpid == -1)
+    errExit("fork");
+  else if (cpid == 0) {
     printf("execl:\n");
-    execl("/bin/ls", "ls", "-l", "-a", "-h", NULL);
-  } else {
-    wait(NULL);
-    rc = fork();
-    if (rc == 0) {
-      printf("\nexeclp:\n");
-      execlp("ls", "ls", "-l", "-a", "-h", NULL);
-    } else {
-      wait(NULL);
-      rc = fork();
-      if (rc == 0) {
-        printf("\nexecle:\n");
-        execle("/bin/ls", "ls", "-l", "-a", "-h", NULL, envp);
-      } else {
-        wait(NULL);
-        rc = fork();
-        if (rc == 0) {
-          printf("\nexecv:\n");
-          execv("/bin/ls", argvs);
-        } else {
-          wait(NULL);
-          rc = fork();
-          if (rc == 0) {
-            printf("\nexecvp:\n");
-            execvp("ls", argvs);
-          } else {
-            wait(NULL);
-            rc = fork();
-            if (rc == 0) {
-              printf("\nexecve:\n");
-              execve("/bin/ls", argvs, envp);
-            } else {
-              wait(NULL);
-#ifdef __GNU_LIBRARY__
-              rc = fork();
-              if (rc == 0) {
-                printf("\nexecvpe:\n");
-                execvpe("ls", argvs, envp);
-              } else
-                wait(NULL);
-#endif
-            }
-          }
-        }
-      }
-    }
+    if (execl("/bin/ls", "ls", "-l", "-a", "-h", (char *)NULL) == -1)
+      errExit("execl");
   }
+
+  if (wait(NULL) == -1)
+    errExit("wait");
+  if ((cpid = fork()) == -1)
+    errExit("fork");
+  else if (cpid == 0) {
+    printf("\nexeclp:\n");
+    if (execlp("ls", "ls", "-l", "-a", "-h", (char *)NULL) == -1)
+      errExit("execlp");
+  }
+
+  if (wait(NULL) == -1)
+    errExit("wait");
+  if ((cpid = fork()) == -1)
+    errExit("fork");
+  else if (cpid == 0) {
+    printf("\nexecle:\n");
+    if (execle("/bin/ls", "ls", "-l", "-a", "-h", (char *)NULL, envp) == -1)
+      errExit("execle");
+  }
+
+  if (wait(NULL) == -1)
+    errExit("wait");
+  if ((cpid = fork()) == -1)
+    errExit("fork");
+  else if (cpid == 0) {
+    printf("\nexecv:\n");
+    if (execv("/bin/ls", argv) == -1)
+      errExit("execv");
+  }
+
+  if (wait(NULL) == -1)
+    errExit("wait");
+  if ((cpid = fork()) == -1)
+    errExit("fork");
+  else if (cpid == 0) {
+    printf("\nexecvp:\n");
+    if (execvp("ls", argv) == -1)
+      errExit("execvp");
+  }
+
+  if (wait(NULL) == -1)
+    errExit("wait");
+  if ((cpid = fork()) == -1)
+    errExit("fork");
+  else if (cpid == 0) {
+    printf("\nexecve:\n");
+    if (execve("/bin/ls", argv, envp) == -1) // system call
+      errExit("execve");
+  }
+
+#ifdef __GNU_LIBRARY__
+  if (wait(NULL) == -1)
+    errExit("wait");
+  if ((cpid = fork()) == -1)
+    errExit("fork");
+  else if (cpid == 0) {
+    printf("\nexecvpe:\n");
+    if (execvpe("ls", argv, envp) == -1)
+      errExit("execvpe");
+  }
+
+  if (wait(NULL) == -1)
+    errExit("wait");
+  if ((cpid = fork()) == -1)
+    errExit("fork");
+  else if (cpid == 0) {
+    printf("\nfexecve:\n");
+    int fd = 0;
+    if ((fd = open("/bin/ls", O_PATH)) == -1)
+      errExit("open");
+    if (fexecve(fd, argv, envp) == -1)
+      errExit("fexecve");
+  }
+#endif
 
   return 0;
 }
