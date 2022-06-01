7dd308c00f42058b7317c49c3c0584861c2f2130
xxyzz <gitpull@protonmail.com>
2022-06-01 13:16:19 +0800

    TLB: fix hwloc-bind argument and update images
    I didn't use `hwloc-bind` correctly before, pass the `--single` argument alone
    does nothing.

--- 19/README.md
+++ 19/README.md
@@ -12,17 +12,17 @@
 
 3. Now write a script in your favorite scripting language (bash?) to run this program, while varying the number of pages accessed from 1 up to a few thousand, perhaps incrementing by a factor of two per iteration. Run the script on different machines and gather some data. How many trials are needed to get reliable measurements?
 
-    10000 trials.
+    100000 trials.
 
 4. Next, graph the results, making a graph that looks similar to the one above. Use a good tool like `ploticus` or even `zplot`. Visualization usually makes the data much easier to digest; why do you think that is?
 
     ```
     $ make
-    $ python plot.py 14 10000 --single_cpu
+    $ python plot.py 14 100000 --single_cpu
     ```
 
-    ![Linux_single](https://user-images.githubusercontent.com/21101839/145330207-4722c521-382c-4bea-aac3-cff9eb521300.png)
-    ![Darwin_single](https://user-images.githubusercontent.com/21101839/145330196-45cd0383-2e55-45b2-8b60-c19051eebfcd.png)
+    ![Linux_100000_single](https://user-images.githubusercontent.com/21101839/171332561-c1486739-5f25-4490-b55d-c69702a22a60.png)
+    ![Darwin_100000](https://user-images.githubusercontent.com/21101839/171332431-e321dbd2-c650-48d4-a14f-77d3d2db03e2.png)
 
     >Which organ provides the greatest bandwidth in terms of its access to the human brain? Obviously, the eyeball.
     >
@@ -34,9 +34,9 @@
 
 6. Another thing to watch out for is the fact that most systems today ship with multiple CPUs, and each CPU, of course, has its own TLB hierarchy. To really get good measurements, you have to run your code on just one CPU, instead of letting the scheduler bounce it from one CPU to the next. How can you do that? (hint: look up “pinning a thread” on Google for some clues) What will happen if you don’t do this, and the code moves from one CPU to the other?
 
-    Use `sched_setaffinity(2)`, `pthread_setaffinity_np(3)`, `taskset(1)` or `sudo systemd-run -p AllowedCPUs=0` on Linux, `cpuset_setaffinity(2)` or `cpuset(1)` on FreeBSD.
+    Use `sched_setaffinity(2)`, `pthread_setaffinity_np(3)`, `taskset(1)` or `sudo systemd-run -p AllowedCPUs=0 ./tlb.out` on Linux, `cpuset_setaffinity(2)` or `cpuset(1)` on FreeBSD.
 
-    Or use `hwloc-bind --single`.
+    Or use `hwloc-bind package:0.pu:0 -- ./tlb.out`.
 
 7. Another issue that might arise relates to initialization. If you don’t initialize the array `a` above before accessing it, the first time you access it will be very expensive, due to initial access costs such as demand zeroing. Will this affect your code and its timing? What can you do to counterbalance these potential costs?
 
--- 19/plot.py
+++ 19/plot.py
@@ -9,42 +9,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('pages', type=int, help='input 4, 1 to 2^3 pages')
-parser.add_argument('trials')
-parser.add_argument('--single_cpu', action='store_true')
+parser.add_argument("pages", type=int, help="input 4, 1 to 2^3 pages")
+parser.add_argument("trials")
+parser.add_argument("--single_cpu", action="store_true")
 args = parser.parse_args()
+
+if platform.system() == "Darwin":
+    args.single_cpu = False
 
 data = []
 x = np.arange(args.pages)
-pages = 2 ** x
+pages = 2**x
 for i in pages:
     if args.single_cpu:
-        if shutil.which('hwloc-bind'):
+        if shutil.which("taskset"):
             r = subprocess.run(
-                ['hwloc-bind', '--single', './tlb.out', str(i), args.trials],
-                capture_output=True, check=True, text=True)
-        elif shutil.which('taskset'):
+                ["taskset", "-c", "0", "./tlb.out", str(i), args.trials],
+                capture_output=True,
+                check=True,
+                text=True,
+            )
+        elif shutil.which("hwloc-bind"):
             r = subprocess.run(
-                ['taskset', '-c', '0', './tlb.out', str(i), args.trials],
-                capture_output=True, check=True, text=True)
+                [
+                    "hwloc-bind",
+                    "package:0.pu:0",
+                    "--",
+                    "./tlb.out",
+                    str(i),
+                    args.trials,
+                ],
+                capture_output=True,
+                check=True,
+                text=True,
+            )
         else:
             raise Exception("Can't find hwloc-bind or taskset")
     else:
-        r = subprocess.run(['./tlb.out', str(i), args.trials],
-                           capture_output=True, check=True, text=True)
+        r = subprocess.run(
+            ["./tlb.out", str(i), args.trials],
+            capture_output=True,
+            check=True,
+            text=True,
+        )
     data.append(float(r.stdout))
 
-plt.plot(x, data, marker='o', color='orange')
+plt.plot(x, data, marker="o", color="orange")
 plt.margins(0)
-plt.xticks(x, pages, fontsize='x-small')  # evenly spaced
-plt.xlabel('Number Of Pages')
-plt.ylabel('Time Per Access (ns)')
-title = 'TLB Size Measurement: '
-title += f'{platform.system()} {platform.release()} {platform.machine()}'
+plt.xticks(x, pages, fontsize="x-small")  # evenly spaced
+plt.xlabel("Number Of Pages")
+plt.ylabel("Time Per Access (ns)")
+title = "TLB Size Measurement: "
+title += f"{platform.system()} {platform.release()} {platform.machine()}"
 if args.single_cpu:
-    title += ' single CPU'
+    title += " single CPU"
 plt.title(title)
 plt.savefig(
-    f'{platform.system()}{"_single.png" if args.single_cpu else ".png"}',
-    dpi=227)
+    f'{platform.system()}_{args.trials}{"_single.png" if args.single_cpu else ".png"}', dpi=227
+)
 plt.show()  # pacman -S tk
