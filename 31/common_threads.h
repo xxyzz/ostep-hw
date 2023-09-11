1ef7fbb29a7a7e98eef352fac0e750e3a01d70a5
czg <czgsci@163.com>
2023-09-11 12:17:35 +0800

    use implicit check in sem_open instead of check_shm

--- 31/common_threads.h
+++ 31/common_threads.h
@@ -6,11 +6,9 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <sys/stat.h> // For mode constants
-#include <string.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 
 #define Pthread_create(thread, attr, start_routine, arg)                       \
   assert(pthread_create(thread, attr, start_routine, arg) == 0)
@@ -42,28 +40,21 @@
 
 /*
 If some programs created sem by sem_open but not destroyed it maybe due to the deadlock and Ctrl-C or others,
-then the next time "Sem_open" will skip the create operation and not init as expected.
-Here assume that sem is accidentally stored in /dev/shm/ with format like /dev/shm/sem.foo
+then with "O_CREAT" flag, the next time "Sem_open" will skip the create operation and not init as expected.
 */
-void check_shm(char *obj_name){
-  char *shm_path = (char *)malloc(100*sizeof(char));
-  strcpy(shm_path,"/dev/shm/sem.");
-  strcat(shm_path,obj_name+1);
-  if (access(shm_path,F_OK)==0)
-    Sem_unlink(obj_name);
-  free(shm_path);
-}
-
 
 sem_t *Sem_open(char *name, int value) {
   sem_t *sem;
-  check_shm(name);
-  /*
-  "O_CREAT|O_EXCL" also fine. If "EEXIST" then we recall "Sem_open".
-  This has more overheads when failure but less overheads when success.
-  */
-  sem = sem_open(name, O_CREAT, S_IRWXU, value);
-  assert(sem != SEM_FAILED);
+  sem = sem_open(name, O_CREAT|O_EXCL, S_IRWXU, value);
+  if (sem == SEM_FAILED){
+    if (errno==EEXIST){
+      Sem_unlink(name);
+      sem = Sem_open(name, value);
+    }else{
+      fprintf(stderr,"sem_open error");
+      exit(-1);
+    }
+  }
   return sem;
 }
 
